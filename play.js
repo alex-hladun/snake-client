bf5d643153d306ff310cdcdc23e5cb88d3daa8dc
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-06-02 22:31:43 -0600

    Added name and moved connection information to client.js

--- play.js
+++ play.js
@@ -1,29 +1,6 @@
 const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: '<IP ADDRESS HERE>',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
-  return conn;
-};
-
-console.log('Connecting ...');
-connect();
-
+const connect = require('./client');
+connect.connect();
 
 //////
 
