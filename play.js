dab5107a33b61c784b854a2427c64d505660ea49
alex-hladun <alexhladun@gmail.com>
2020-06-03 02:11:06 +0000

    Initial play file.

---
+++ play.js
@@ -0,0 +1,30 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '<IP ADDRESS HERE>',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  conn.on('connect', () => {
+    conn.write('Hello from client!');
+  });
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
+
+
+//////
+
+// client.js
