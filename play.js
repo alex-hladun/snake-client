09716863e58cc2242943c132e2557a3e12a3c87b
alex-hladun <alexhladun@gmail.com>
2020-06-03 05:11:03 +0000

    Made change

--- play.js
+++ play.js
@@ -1,7 +1,11 @@
-const net = require('net');
+// const net = require('net');
 const connect = require('./client');
-connect.connect();
+const { setupInput } = require('./input');
 
-//////
+// "Update play.js to send the connection object returned from conect
+// into our setupInput function"
+setupInput(connect.connect());
 
+
+// Runs the input collection, through handleuserinput.
 // client.js
